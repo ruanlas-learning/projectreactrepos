9da9365d2a97c4fc21808c47c9c050d14bfa1f86
Ruan Silva <ruan.silva@widesoftware.com.br>
2019-11-05 15:53:32 -0300

    Carregando detalhes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Container } from './styles';
+import api from '../../services/api';
 
+// {decodeURIComponent(match.params.repositorio)}
 export default function Repositorio({match}){
+
+    const [repositorio, setRepositorio] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        async function load(){
+            const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${nomeRepo}`),
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+
+            // console.log(repositorioData.data);
+            // console.log(issuesData.data);
+            setRepositorio(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+        }
+
+        load();
+    }, []);
+
     return(
         <div>
-            <h1 style={{color: '#FFF'}}>
-                {decodeURIComponent(match.params.repositorio)}
-            </h1>
+            <Container>
+                
+            </Container>
         </div>
     );
 }
