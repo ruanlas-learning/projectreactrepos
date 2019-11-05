5819541639f1c703eb454f31c12631fc71ca4a47
Ruan Silva <ruan.silva@widesoftware.com.br>
2019-11-05 11:33:47 -0300

    Estilizando pagina main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {Title} from './styles';
+import {FaGithub, FaPlus} from 'react-icons/fa';
+import {Container, Form, SubmitButton} from './styles';
 
 export default function Main(){
     return(
-        <div>
-            <Title>Main</Title>
-        </div>
+        <Container>
+            <h1> 
+                <FaGithub size={25}/>
+                Meus Repositórios
+            </h1>
+
+            <Form onSubmit={() => { } }>
+                <input type="text" placeholder="Adicionar Repositorios" />
+
+                <SubmitButton>
+                    <FaPlus color="#FFF" size={14}/>
+                </SubmitButton>
+            </Form>
+        </Container>
     );
 }
